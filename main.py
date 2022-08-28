4d04c7de8813ec6c9ac4c2c18779456501b67ae8
emilmani7 <104570777+emilmani7@users.noreply.github.com>
2022-08-28 13:52:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,14 +12,16 @@ def predict():
     with open('./model_files/model.bin', 'rb') as f_in:
         model = pickle.load(f_in)
         f_in.close()
-    predictions = predict_mpg(vehicle, model)
+    predictions = predict_mpg(vehicle_config, model)
 
-    result = {
+    response = {
         'mpg_prediction': list(predictions)
     }
-    return jsonify(result)
+    return jsonify(response)
 
-@app.route('/ping', methods=['GET'])
-def ping():
-    return "Pinging Model!!"
+#@app.route('/ping', methods=['GET'])
+#def ping():
+    #return "Pinging Model!!"
+ If__name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=9696)
 
